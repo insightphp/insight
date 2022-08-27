e700c800377a44429f617c9c4faa8d860de178a9
Peter Stovka <stovka.peter@gmail.com>
2022-08-27 14:52:01 +0200

    wip

---
+++ bin/publish_npm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+npm run build
+npm pack
+npm publish --access public
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Insight',
+  description: 'Documentation for Insight.',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Getting Started',
+        items: [
+          { text: 'Installation', link: '/installation.html' },
+          { text: 'Release notes', link: '/release-notes.html' }
+        ]
+      },
+    ]
+  }
+}
