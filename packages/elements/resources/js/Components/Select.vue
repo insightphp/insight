684088fe87ba6cc79c4aab17dccae06f89fc55b4
Peter Stovka <stovka.peter@gmail.com>
2022-09-12 15:50:09 +0200

    wip

--- packages/elements/resources/js/Components/Select.vue
+++ packages/elements/resources/js/Components/Select.vue
@@ -15,6 +15,12 @@
               </svg>
             </button>
           </div>
+          <p v-if="maxLimitEnabled && maximumSelected" class="px-2 text-warning-700 text-xs inline-flex items-center font-medium mb-2 -mt-1">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4 mr-1">
+              <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+            {{ maxOptionsSelectedLabel }}
+          </p>
         </slot>
       </ListboxButton>
       <Transition enter-from-class="opacity-0" enter-to-class="opacity-100" enter-active-class="transition-opacity duration-150" leave-active-class="transition-opacity duration-150" leave-from-class="opacity-100" leave-to-class="opacity-0">
@@ -30,7 +36,7 @@
           </div>
 
           <template v-if="filteredOptions.length > 0">
-            <ListboxOption v-for="option in filteredOptions" :key="option[by]" :value="option" as="template" v-slot="{ active, selected }">
+            <ListboxOption v-for="option in filteredOptions" :key="option[by]" :value="option" as="template" v-slot="{ active, selected }" :disabled="maximumSelected && !isOptionSelected(option)">
               <slot name="option" v-bind="{ active, selected, option, labelBy }">
                 <SelectOption :show-icon="showOptionIcon" :active="active" :selected="selected" :option="option" :label-by="labelBy" />
               </slot>
@@ -102,6 +108,8 @@ const props = withDefaults(defineProps<{
   searchPlaceholder?: string
   searchable?: boolean
   searchableBy?: string
+  maxOptionsSelectedLabel?: string
+  max?: number
 }>(), {
   by: 'value',
   labelBy: 'label',
@@ -113,8 +121,21 @@ const props = withDefaults(defineProps<{
   noResultsFoundLabel: 'No results found.',
   searchPlaceholder: 'Search…',
   searchable: false,
-  searchableBy: 'label'
+  searchableBy: 'label',
+  max: -1,
+  maxOptionsSelectedLabel: 'Maximum options selected.'
 })
+
+const maxLimitEnabled = computed(() => props.multiple && props.max > 0)
+const maximumSelected = computed(() => maxLimitEnabled.value && (selectedValue.value as Array<Option>).length >= props.max)
+
+const isOptionSelected = (option: Option) => {
+  if (props.multiple) {
+    return !!(selectedValue.value as Array<Option>).find(it => resolveIdentifier(option) == resolveIdentifier(it))
+  }
+
+  return resolveIdentifier(option) == resolveIdentifier(selectedValue.value)
+}
 
 const hasError = computed(() => !!props.error)
 const isMultiple = computed(() => !!props.multiple)
